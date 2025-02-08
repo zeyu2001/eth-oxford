0b1002c9ff162d8dc8be36257a4dde2c5870aa5f
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2025-02-08 11:21:00 +0000

    feat: pull request

--- src/server/api/routers/scanner.ts
+++ src/server/api/routers/scanner.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { cloneRepository } from "@/lib/github";
+import {
+  cloneRepository,
+  commitFile,
+  createBranch,
+  createPR,
+} from "@/lib/github";
+import { spawnSync } from "node:child_process";
 
 export const scannerRouter = createTRPCRouter({
   scanRepository: publicProcedure
@@ -9,19 +15,120 @@ export const scannerRouter = createTRPCRouter({
       z.object({
         installationId: z.number(),
         repositoryId: z.string(),
-      })
+        username: z.string(),
+      }),
     )
-    .mutation(async ({ input }) => {
+    .mutation(async ({ ctx, input }) => {
       try {
-        // Ensure cloneRepository handles errors or is wrapped in a try-catch if needed
-        const tempDir = await cloneRepository(input.installationId, input.repositoryId);
+        const tempDir = await cloneRepository(
+          input.installationId,
+          input.repositoryId,
+        );
 
-        console.log(tempDir); // Consider removing this in production code for better logging
+        const { stdout, stderr } = spawnSync("semgrep", [
+          "scan",
+          "--config=auto",
+          tempDir,
+          "--json",
+        ]);
 
-        return { tempDir }; // Return in an object if you plan to add more fields later
+        const vulnerabilitySchema = z.object({
+          path: z.string(),
+          start: z.object({
+            line: z.number(),
+            col: z.number(),
+          }),
+          end: z.object({
+            line: z.number(),
+            col: z.number(),
+          }),
+          extra: z.object({
+            severity: z.string(),
+            message: z.string(),
+            lines: z.string(),
+          }),
+        });
+
+        const semgrepOutputSchema = z.object({
+          results: z.array(vulnerabilitySchema),
+        });
+
+        let formattedResults = [];
+        try {
+          const scanResults = semgrepOutputSchema.parse(
+            JSON.parse(stdout.toString()),
+          );
+          formattedResults = scanResults.results.map((vuln) => ({
+            file: vuln.path,
+            startLine: vuln.start.line,
+            startCol: vuln.start.col,
+            endLine: vuln.end.line,
+            endCol: vuln.end.col,
+            severity: vuln.extra.severity,
+            message: vuln.extra.message,
+            code: vuln.extra.lines,
+          }));
+        } catch {
+          throw new Error("Failed to parse Semgrep output.");
+        }
+
+        const scan = await ctx.db.scan.create({
+          data: {
+            username: input.username,
+            repositoryId: input.repositoryId,
+            result: {
+              create: formattedResults,
+            },
+          },
+        });
+
+        return scan;
       } catch (error) {
         console.error("Failed to clone repository:", error);
         throw new Error("Repository scan failed.");
       }
+    }),
+
+  createPullRequest: publicProcedure
+    .input(
+      z.object({
+        installationId: z.number(),
+        repositoryId: z.string(),
+        title: z.string(),
+        body: z.string(),
+        currSHA: z.string(),
+        filepath: z.string(),
+        newFileContent: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const branchName = `security/codecure-${input.title.toLowerCase().replace(" ", "-")}`;
+
+      const result = await createBranch(
+        input.installationId,
+        input.repositoryId,
+        branchName,
+        input.currSHA,
+      );
+
+      const commitResult = await commitFile(
+        input.installationId,
+        input.repositoryId,
+        branchName,
+        input.filepath,
+        input.newFileContent,
+        `Add security vulnerability fix at ${input.filepath}`,
+      );
+
+      const prResult = await createPR(
+        input.installationId,
+        input.repositoryId,
+        input.title,
+        input.body,
+        branchName,
+        "main",
+      );
+
+      return prResult;
     }),
 });
